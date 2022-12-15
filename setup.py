767b168bae738a8383b69e67cb636b0da6458231
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2022-12-15 14:28:45 +0100

    apply diff from https://trac-hacks.org/ticket/14086

--- setup.py
+++ setup.py
@@ -12,13 +12,16 @@ setup(
     name = 'TracMermaid',
     version = '0.4.2',
     packages = find_packages(exclude=['*.tests*']),
-    license = "BSD 3-Clause",
+    license = 'BSD 3-Clause',
+    description = 'Render Mermaid diagrams in Trac wiki pages',
+    keywords = 'diagram gantt graph mermaid',
     entry_points = {
         'trac.plugins': [
             'tracmermaid = tracmermaid',
         ],
     },
-    install_requires=['Trac'],
+    classifiers = ['Framework :: Trac'],
+    install_requires = ['Trac'],
     package_data = {
         'tracmermaid': [
             'htdocs/*',
